0bb11593ba2ae91665fa9ddc0866e3fb0f82e847
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-03-09 20:45:30 -0400

    modificacion del main

--- RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/main.cpp
@@ -3,19 +3,22 @@
 #include "Rayo.h"
 #include "Camara.h"
 
-int nx = 480;
-int ny = 360; 
+float nx = 640.0;
+float ny = 480.0; 
+Vector e(1,2,-6);
+Vector center(1,2,-4);
+Vector up(0,1,0);
+float fov = 39.0;
+float df = 1.0;
 
 int main(){
-	
-
-	for(int i=0; i<=nx;i++){
-		for(int j=0;j<=ny;j++){
-
+	Camara camera(e, center, up, fov, df);
+	Vector dir;
+	for(float i=0; i<=nx;i++){
+		for(float j=0;j<=ny;j++){
+			dir = camera.direccionRayo(i,j,nx,ny);
+			dir.show();
 		}
 	}
-
-
-
 	return 0;
 }
