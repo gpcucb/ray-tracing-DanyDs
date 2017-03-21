496a0058f0c03686b8ccdc8203fa6aa7814857ac
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-03-21 18:39:08 -0400

    Render class and Raytracer class added

---
+++ RayTracing-DanyDs/Render.cpp
@@ -0,0 +1,30 @@
+#include "Render.h"
+
+
+Render::Render()
+{
+}
+
+
+Render::Render(float width, float height,RGB background_color(float background_red,float background_green,float background_blue))
+{
+	this->width=width;
+	this->height=height;
+	this->background_color = background_color; 
+}
+
+Render::~Render()
+{
+}
+
+
+
+void reset();
+	void calculate_pixel(float i,float j);
+	void render(string title, string file_name);
+protected:
+	void init_gl();
+	void draw_scene();
+	void reshape();
+	void idle();
+	void save_image(string file_name);
